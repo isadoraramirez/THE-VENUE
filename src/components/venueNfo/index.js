570a92020846c83a71712e1cf1dd692c8b0cd66e
doctoramalvada <mydemoni@hotmail.com>
2019-06-29 22:00:28 -0500

    zoom en venuenfo

--- src/components/venueNfo/index.js
+++ src/components/venueNfo/index.js
@@ -1,63 +1,64 @@
 import React from 'react';
-import icon_calendar from '../../resources/images/icons/calendar.png'
-import icon_location from '../../resources/images/icons/location.png'
-//import Zoom from 'react_reveal/Zoom';
+import Zoom from 'react-reveal/Zoom';
 
-const VenueNfo = () => {
+import icon_calendar from '../../resources/images/icons/calendar.png';
+import icon_location from '../../resources/images/icons/location.png';
+
+const VunueNfo = () => {
     return (
         <div className="bck_black">
-          <div className="center_wrapper">
-              <div className="vn_wrapper">
-                  <div className="vn_item">
-                      <div className="vn_outer">
-                          <div className="vn_inner">
-                              <div className="vn_icon_square bck_red">
-                                  </div>
-                                  <div className="vn_icon"
-                                  style={{
-                                      background:`url(${icon_calendar})`
-                                  }}>
-                                  </div>
-                                  <div className="vn_title">
-                                      Event Date & Time
+            <div className="center_wrapper">
+                <div className="vn_wrapper">
 
-                                  </div>
-                                  <div className="vn_desc">
-                                      7 September @6:30 pm
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
+                    <Zoom duration={500}>
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_red"></div>
+                                    <div 
+                                        className="vn_icon"
+                                        style={{
+                                            background:`url(${icon_calendar})`
+                                        }}
+                                    ></div>
+                                    <div className="vn_title">
+                                        Event Date & Time
+                                    </div>
+                                    <div className="vn_desc">
+                                    7 September @6:30 pm
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                      <div className="vn_item">
-                      <div className="vn_outer">
-                          <div className="vn_inner">
-                              <div className="vn_icon_square bck_yellow">
-                                  </div>
-                                  <div className="vn_icon"
-                                  style={{
-                                      background:`url(${icon_location})`
-                                  }}>
-                                  </div>
-                                  <div className="vn_title">
-                                      Event Location
-
-                                  </div>
-                                  <div className="vn_desc">
-                                  Amphitheatre, Englewood, CO
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-
-
-                  
-                  
-                  </div>
-              </div>  
+                    </Zoom>
+                    
+                    <Zoom duration={500} delay={500}>
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_yellow"></div>
+                                    <div 
+                                        className="vn_icon"
+                                        style={{
+                                            background:`url(${icon_location})`
+                                        }}
+                                    ></div>
+                                    <div className="vn_title">
+                                        Event Location
+                                    </div>
+                                    <div className="vn_desc">
+                                    Amphitheatre, Englewood, CO
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Zoom>
+                    
+                </div>
+            </div>
         </div>
-
     );
 };
 
-export default VenueNfo;
+export default VunueNfo;
