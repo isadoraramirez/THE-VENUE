94109bb7a15a89b0dc5ce8f1b4536e3b1ecc763f
doctoramalvada <mydemoni@hotmail.com>
2019-07-02 12:08:29 -0500

    Updates

---
+++ precache-manifest.1e155a3bee057f51758dace131f24069.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506afcc7cfa766dba8f44ae790846e48",
+    "url": "/isadoraramirez/THE-VENUE/index.html"
+  },
+  {
+    "revision": "e7a8c7faad8408abe564",
+    "url": "/isadoraramirez/THE-VENUE/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "becfe7b73e1c07ad82ec",
+    "url": "/isadoraramirez/THE-VENUE/static/js/2.525b8250.chunk.js"
+  },
+  {
+    "revision": "e7a8c7faad8408abe564",
+    "url": "/isadoraramirez/THE-VENUE/static/js/main.990bbee1.chunk.js"
+  },
+  {
+    "revision": "44610bca5def7a8f3e0d",
+    "url": "/isadoraramirez/THE-VENUE/static/js/runtime~main.5abc403c.js"
+  },
+  {
+    "revision": "b5b4067e64bd09409001dc674a5becdb",
+    "url": "/isadoraramirez/THE-VENUE/static/media/slide_one.b5b4067e.jpg"
+  },
+  {
+    "revision": "2d33038b694b7530e3a1d212bd2256bb",
+    "url": "/isadoraramirez/THE-VENUE/static/media/slide_three.2d33038b.jpg"
+  },
+  {
+    "revision": "ec20af15e555c7dde7d61fef81b6ebb1",
+    "url": "/isadoraramirez/THE-VENUE/static/media/slide_two.ec20af15.jpg"
+  }
+]);
